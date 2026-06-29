3d34b756593d203c5caf2f0833e604525b07982b
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-06-30 01:55:49 +0530

    fix: clean up dead global.d.ts and fix import type in recentStore

--- src/store/recentStore.ts
+++ src/store/recentStore.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
-import { Track } from '../api/jiosaavn';
+import type { Track } from '../api/jiosaavn';
 import { normalizeStoredTrack, sanitizeTrackForStorage } from '../utils/storageTrack';
 import { devWarn } from '../utils/devLog';
 
